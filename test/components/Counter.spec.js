12f90e355691bcf94faf1ec0a58a3e1ac1179b99
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-16 07:49:21 +0000

    テストを修正

--- test/components/Counter.spec.js
+++ test/components/Counter.spec.js
@@ -3,13 +3,13 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Counter from '../../js/components/Counter'
 
-function setup(value = 0) {
+function setup(count = 0) {
   const actions = {
     onClickPlus: expect.createSpy(),
     onClickMinus: expect.createSpy()
   }
   const component = shallow(
-    <Counter value={value} {...actions} />
+    <Counter count={count} {...actions} />
   )
 
   return {
@@ -23,45 +23,18 @@ function setup(value = 0) {
 describe('Counter component', () => {
   it('should display count', () => {
     const { p } = setup()
-    expect(p.text()).toMatch(/^Clicked: 0 times/)
+    expect(p.text()).toMatch(/^Count: 0/)
   })
 
-  it('first button should call onIncrement', () => {
+  it('first button should call onClickPlus', () => {
     const { buttons, actions } = setup()
     buttons.at(0).simulate('click')
-    expect(actions.onIncrement).toHaveBeenCalled()
+    expect(actions.onClickPlus).toHaveBeenCalled()
   })
 
-  it('second button should call onDecrement', () => {
+  it('second button should call onClickMinus', () => {
     const { buttons, actions } = setup()
     buttons.at(1).simulate('click')
-    expect(actions.onDecrement).toHaveBeenCalled()
-  })
-
-  it('third button should not call onIncrement if the counter is even', () => {
-    const { buttons, actions } = setup(42)
-    buttons.at(2).simulate('click')
-    expect(actions.onIncrement).toNotHaveBeenCalled()
-  })
-
-  it('third button should call onIncrement if the counter is odd', () => {
-    const { buttons, actions } = setup(43)
-    buttons.at(2).simulate('click')
-    expect(actions.onIncrement).toHaveBeenCalled()
-  })
-
-  it('third button should call onIncrement if the counter is odd and negative', () => {
-    const { buttons, actions } = setup(-43)
-    buttons.at(2).simulate('click')
-    expect(actions.onIncrement).toHaveBeenCalled()
-  })
-
-  it('fourth button should call onIncrement in a second', (done) => {
-    const { buttons, actions } = setup()
-    buttons.at(3).simulate('click')
-    setTimeout(() => {
-      expect(actions.onIncrement).toHaveBeenCalled()
-      done()
-    }, 1000)
+    expect(actions.onClickMinus).toHaveBeenCalled()
   })
 })
