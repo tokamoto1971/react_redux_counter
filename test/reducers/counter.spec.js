8412eb4ebc47dea481a4df6be3d89e7d4a834355
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-16 07:40:43 +0000

    テストを追加

---
+++ test/reducers/counter.spec.js
@@ -0,0 +1,22 @@
+import expect from 'expect'
+import counter from '../../js/reducers'
+
+describe('reducers', () => {
+  describe('counter', () => {
+    it('should provide the initial state', () => {
+      expect(counter(undefined, {})).toEqual({count: 0})
+    })
+
+    it('should handle INCREMENT action', () => {
+      expect(counter({count: 1}, { type: 'INCREMENT', payload: 1 })).toEqual({count: 2})
+    })
+
+    it('should handle DECREMENT action', () => {
+      expect(counter({count: 1}, { type: 'DECREMENT', payload: 1 })).toEqual({count: 0})
+    })
+
+    it('should ignore unknown actions', () => {
+      expect(counter({count: 1}, { type: 'unknown', payload: 1 })).toEqual({count: 1})
+    })
+  })
+})
