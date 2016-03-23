210cdac69bc1b56b39d38f7390a2029bd759433c
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-23 02:28:51 +0000

    非同期処理にPromiseを導入

--- test/actions/ActionCreators.spec.js
+++ test/actions/ActionCreators.spec.js
@@ -1,7 +1,6 @@
 import expect from 'expect'
 import { incrementAsync, decrementAsync } from '../../js/actions/ActionCreators'
 import { INCREMENT, DECREMENT } from '../../js/actions/ActionTypes';
-
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
@@ -10,30 +9,31 @@ const mockStore = configureMockStore(middlewares)
 
 describe('async actions', () => {
 
-  it('{type: INCREMENT, payload: 123} が約1000ms後に返されるはず', (done) => {
+  it('{type: INCREMENT, payload: 123}とか約1000ms後に返されるはず', (done) => {
 
     const store = mockStore()
 
     store.dispatch(incrementAsync(123))
+    store.dispatch(incrementAsync(456))
     .then(() => {
       const actions = store.getActions()
-      expect(actions).toEqual([{ type: INCREMENT, payload: 123 }])
+      expect(actions).toEqual([{ type: INCREMENT, payload: 123 }, { type: INCREMENT, payload: 456 }])
     })
     .then(done)
     .catch(done)
   })
 
-  it('{type: DECREMENT, payload: 456} が約1000ms後に返されるはず', (done) => {
+  it('{type: DECREMENT, payload: 456}とか約1000ms後に返されるはず', (done) => {
 
     const store = mockStore()
 
+    store.dispatch(decrementAsync(123))
     store.dispatch(decrementAsync(456))
     .then(() => {
       const actions = store.getActions()
-      expect(actions).toEqual([{ type: DECREMENT, payload: 456 }])
+      expect(actions).toEqual([{ type: DECREMENT, payload: 123 }, { type: DECREMENT, payload: 456 }])
     })
     .then(done)
     .catch(done)
-
   })
 })
